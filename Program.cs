05d39887519855eb592cf8074e082448555f70fc
VladimirTkachenko3010 <v.s.tkachenko@student.khai.edu>
2022-11-14 19:13:23 +0200

    program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,46 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Druid druid = new Druid("druid", 1, 500);
+
+            Console.WriteLine(druid.HeroInformation());
+
+            List<IRampage> rampages = new List<IRampage>()
+            {
+                new Hunter(),
+            };
+
+            foreach (var e in rampages)
+            {
+                e.UltraAttack(druid);
+
+                //Console.WriteLine(druid.HeroInformation());
+            }
+
+            Console.WriteLine(druid.HeroInformation());
+
+            foreach (var e in rampages)
+            {
+                e.Recharge();
+
+                //Console.WriteLine(druid.HeroInformation());
+            }
+
+            foreach (var e in rampages)
+            {
+                e.UltraAttack(druid);
+
+                //Console.WriteLine(druid.HeroInformation());
+            }
+            Console.WriteLine(druid.HeroInformation());
+
+            foreach (var e in rampages)
+            {
+                e.UltraAttack(druid);
+
+                //Console.WriteLine(druid.HeroInformation());
+            }
+            Console.WriteLine(druid.HeroInformation());
         }
     }
 }
